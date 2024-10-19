bda109cbc4a767cfccf5b21557f3b065bc04b4a9
24HP7482@nest.edu.mn <24HP7482nest.edu.mn@G304-CC0029.local>
2024-10-19 10:50:14 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,47 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+const Time = () => {
+  const [timer, setTimer] = useState(20);
+
+  const timeIsRun = () => {
+    // setTimer((prev) => prev - 1);
+    setTimer((prev) => (prev > 0 ? prev - 1 : 0));
+  };
+
+  const sec = timer % 60;
+  const minute = Math.floor(timer / 60);
+
+  const runTime = (times) => {
+    if (times <= 9) return `0${timer}`;
+    // if (times < 1) return "0";
+    return times;
+  };
+
+  const addtime = () => {
+    setTimer(timer + 5);
+  };
+  const minTime = () => {
+    setTimer(timer - 5);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(timeIsRun, 900);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="time">
+      <div className="second">Time : {runTime(sec)} seconds</div>
+      <div className="timeButton">
+        <button className="button" onClick={() => addtime()}>
+          5 sec +
+        </button>
+        <button className="button" onClick={() => minTime()}>
+          - 5 sec
+        </button>
+      </div>
     </div>
   );
-}
+};
+export default Time;
